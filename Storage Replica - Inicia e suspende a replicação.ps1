3ce1a206fec281909d4f1e195938369733e12431
Gabriel Luiz <gabrielluiz@msn.com>
2018-12-16 01:31:07 -0200

    Add files via upload

---
+++ Storage Replica - Inicia e suspende a replicação.ps1
@@ -0,0 +1,5 @@
+﻿## Script para inicar a replicação e suspender a replicação do Storage Replica. - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+Sync-SRGroup -Name "REPLICA-SR2" # Inicia a replicação do Storage Replica.
+
+Suspend-SRGroup -Name "REPLICA-SR2" # Suspende a replicação do Storage Replica.
