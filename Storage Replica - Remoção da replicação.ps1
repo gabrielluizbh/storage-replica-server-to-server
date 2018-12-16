2ae905d47e17f489b75cd6ec5d070b234571573e
Gabriel Luiz <gabrielluiz@msn.com>
2018-12-16 01:28:10 -0200

    Add files via upload

---
+++ Storage Replica - Remoção da replicação.ps1
@@ -0,0 +1,8 @@
+﻿# Script para remoção da replicação - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+Get-SRPartnership
+Get-SRPartnership | Remove-SRPartnership  
+Get-SRGroup | Remove-SRGroup
+Get-SRGroup
+
+# Observação: Execute este comando em cada servidor.
