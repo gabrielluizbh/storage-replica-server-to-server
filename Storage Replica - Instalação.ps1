1b10dc454ee4d6a8c7cbc321bf1bc3a38bda0a2d
Gabriel Luiz <gabrielluiz@msn.com>
2018-12-16 01:24:54 -0200

    Add files via upload

---
+++ Storage Replica - Instalação.ps1
@@ -0,0 +1,14 @@
+﻿## Script de instalação da Função de File Service e o Recurso de Storage Replica - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+$Servers = 'SR1','SR2' # Host name dos servidores.
+
+$Servers | ForEach { Install-WindowsFeature -ComputerName $_ -Name Storage-Replica,FS-FileServer -IncludeManagementTools -restart } # Instalação da Função de File Service e o Recurso de Storage Replica.
+
+
+# Observações:
+
+# Este comando deve ser executado no segundo servidor no caso o servidor com o hostname SR2.
+
+# A Função de (File Service) Servidor de Arquivos só é necessária para Test-SRTopology funcionar, pois abre as portas de firewall necessárias para teste.
+
+# Ao final da instalação ambos servidores serão reinicializados.
