96063cc5f4bbeecd3ace830a4443755afaf463f2
Gabriel Luiz <gabrielluiz@msn.com>
2019-04-17 00:18:09 -0300

    Update Storage Replica - Remoção da replicação.ps1

--- Storage Replica - Remoção da replicação.ps1
+++ Storage Replica - Remoção da replicação.ps1
@@ -1,8 +1,10 @@
 ﻿# Script para remoção da replicação - Créditos Gabriel Luiz - www.gabrielluiz.com ##
 
 Get-SRPartnership
-Get-SRPartnership | Remove-SRPartnership  
+Get-SRPartnership | Remove-SRPartnership -force -IgnoreRemovalFailure
 Get-SRGroup | Remove-SRGroup
 Get-SRGroup
 
-# Observação: Execute este comando em cada servidor.
+# Observações:
+# Execute este comando em cada servidor para remover a replicação.
+# Em um ambinete de produção execute somente Get-SRPartnership | Remove-SRPartnership com os complementos -force -IgnoreRemovalFailure se você perder a comunição como servidor SR1, isso é quando ocorrer um desatre, caso contrário o servidor SR2 pode continuar acreditando que está em uma pareceria (replicação).
